2d7ecc79f1eed10e05dec99ec92523f6e206af37
rodaebel <tobias.rodaebel@googlemail.com>
2011-05-25 18:00:21 +0200

    Augmenting test data with random words.

--- mkdata.py
+++ mkdata.py
@@ -6,6 +6,17 @@ import sys
 
 CHARS = "".join(chr(i) for i in range(32, 127))
 
+WORDS = [
+    ("Hello", 5),
+    ("Doctor", 6),
+    ("Name", 4),
+    ("continue", 8),
+    ("yesterday", 9),
+    ("Tomorrow", 8)
+]
+
+NUM_WORDS = len(WORDS)
+
 
 if __name__ == "__main__":
 
@@ -37,7 +48,13 @@ if __name__ == "__main__":
         buf = StringIO.StringIO()
 
         for i in range(c):
-            print >>buf, "".join(r.sample(chars, length))
+            if random.randrange(2):
+                w, l = WORDS[random.randrange(NUM_WORDS)]
+                x = random.randrange(0, length - l - 2)
+                print >>buf, "".join(r.sample(chars, x)), w, \
+                             "".join(r.sample(chars, length - x - l - 2))
+            else:
+                print >>buf, "".join(r.sample(chars, length))
 
         sys.stdout.write(buf.getvalue())
 
